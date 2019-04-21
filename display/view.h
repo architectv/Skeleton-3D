fa69edcdbdeb17697b60877a342d224b236a2410
Kerimov Ahmed <kerimov.dev@yandex.ru>
2019-04-21 15:13:41 +0300

    Add scene and view headers

---
+++ display/view.h
@@ -0,0 +1,20 @@
+#ifndef VIEW_H_
+#define VIEW_H_
+
+#include "scene.h"
+// #include "camera.h"
+// #include "entity/surface.h"
+
+class View {
+ public:
+
+  void DrawScene();
+  void DrawModel(const Model& model);
+  void DrawSurface(const Surface& surface);
+
+ private:
+  Scene scene_;
+  Camera camera_;
+};
+
+#endif  // VIEW_H_
