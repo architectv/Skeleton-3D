70e85bb38e206e1995d6617eed84826535dc2afa
Alexey Vasyukov <vasyukov1999al@yandex.ru>
2019-04-21 15:34:07 +0300

    CurveCreator as Factory Method

---
+++ entity/creator/curvecreator.h
@@ -0,0 +1,14 @@
+#ifndef CURVECREATOR_H
+#define CURVECREATOR_H
+
+#include <entity/edge.h>
+
+class CurveCreator: public Edge {
+ public:
+    Edge CreateLine(Point, Point);
+    vector<Edge> CreateRectangle(Point, Point);
+    vector<Edge> CreateEllipse(Point, double, double);
+    vector<Edge> CreatePolygon(Point, double, int);
+};
+
+#endif // CURVECREATOR_H
