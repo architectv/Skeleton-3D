555b7d968f1f2497e5ed5c5a0eb7aeec19ab3a4f
Kerimov Ahmed <kerimov.dev@yandex.ru>
2019-05-18 15:59:06 +0300

    Add matrix

---
+++ math3d/matrix.h
@@ -0,0 +1,201 @@
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+#include <initializer_list>
+#include <istream>
+#include <ostream>
+#include "vector.h"
+
+template <std::size_t rows, std::size_t cols>
+class Matrix;
+
+using Matrix3 = Matrix<3, 3>;
+using Matrix4 = Matrix<4, 4>;
+
+template <std::size_t rows, std::size_t cols>
+class Matrix {
+  static_assert(rows != 0 && cols != 0, "Zero dimension matrix does not exist");
+
+ public:
+  using row = Vector<rows>;
+  using reference = row&;
+  using const_reference = const row&;
+
+  constexpr Matrix() noexcept = default;
+  constexpr Matrix(const std::initializer_list<float>& list) noexcept;
+  constexpr Matrix(float scalar);
+  Matrix(const Vector<cols>& col);
+
+  constexpr static Matrix identify() noexcept;
+
+  reference operator[](std::size_t row);
+  constexpr const_reference operator[](std::size_t row) const;
+
+  reference at(std::size_t row);
+  constexpr const_reference at(std::size_t row) const;
+
+  constexpr Matrix& operator+=(const Matrix& other);
+  constexpr Matrix& operator-=(const Matrix& other);
+  template <std::size_t span> constexpr Matrix<rows, span>& operator*=(const Matrix<cols, span>& other);
+  constexpr Matrix& operator*=(float scalar);
+
+  template <std::size_t rows_, std::size_t cols_>
+  friend constexpr Matrix<rows_, cols_> operator+(const Matrix<rows_, cols_>& m1, const Matrix<rows_, cols_>& m2);
+
+  template <std::size_t rows_, std::size_t cols_>
+  friend constexpr Matrix<rows_, cols_> operator-(const Matrix<rows_, cols_>& m1, const Matrix<rows_, cols_>& m2);
+
+  template <std::size_t rows_, std::size_t cols_, std::size_t span_>
+  friend constexpr Matrix<rows_, span_> operator*(const Matrix<rows_, cols_>& m1, const Matrix<cols_, span_>& m2);
+
+  template <std::size_t rows_, std::size_t cols_>
+  friend constexpr Matrix<rows_, cols_> operator*(const Matrix<rows_, cols_>& matrix, float scalar);
+
+  template <std::size_t rows_, std::size_t cols_>
+  friend constexpr Vector<rows_ - 1> operator*(const Matrix<rows_, cols_>& matrix, const Vector<rows_ - 1>& vector);
+
+  template <std::size_t rows_, std::size_t cols_>
+  friend std::istream& operator>>(std::istream& is, Matrix<rows_, cols_>& matrix);
+
+  template <std::size_t rows_, std::size_t cols_>
+  friend std::ostream& operator<<(std::ostream& os, const Matrix<rows_, cols_>& matrix);
+
+ protected:
+  row data_[cols];
+};
+
+template <std::size_t rows, std::size_t cols>
+constexpr Matrix<rows, cols>::Matrix(const std::initializer_list<float>& list) noexcept: Matrix() {
+  auto it = list.begin();
+  for (std::size_t i = 0; i != rows; ++i)
+    for (std::size_t j = 0; j != cols; ++j)
+      data_[i][j] = *it++;
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr Matrix<rows, cols>::Matrix(float scalar) {
+  static_assert(rows == cols, "Must not be called on a rectangular matrix");
+  for (std::size_t i = 0; i != rows; ++i)
+    data_[i][i] = scalar;
+}
+
+template <std::size_t rows, std::size_t cols>
+Matrix<rows, cols>::Matrix(const Vector<cols>& col) {
+  static_assert(cols == 1, "Must not be called on a matrix");
+  for (std::size_t i = 0; i != cols; ++i)
+    data_[i][0] = col[i];
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr Matrix<rows, cols> Matrix<rows, cols>::identify() noexcept {
+  Matrix<rows, cols> identify(1);
+  return identify;
+}
+
+template <std::size_t rows, std::size_t cols>
+typename Matrix<rows, cols>::reference Matrix<rows, cols>::operator[](std::size_t row) {
+  assert(row < rows);
+  return data_[row];
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr typename Matrix<rows, cols>::const_reference Matrix<rows, cols>::operator[](std::size_t row) const {
+  assert(row < rows);
+  return data_[row];
+}
+
+template <std::size_t rows, std::size_t cols>
+typename Matrix<rows, cols>::reference Matrix<rows, cols>::at(std::size_t row) {
+  if (row >= rows)
+    throw std::out_of_range("Out of range");
+  return data_[row];
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr typename Matrix<rows, cols>::const_reference Matrix<rows, cols>::at(std::size_t row) const {
+  if (row >= rows)
+    throw std::out_of_range("Out of range");
+  return data_[row];
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr Matrix<rows, cols>& Matrix<rows, cols>::operator+=(const Matrix<rows, cols>& other) {
+  for (std::size_t i = 0; i != rows; ++i)
+    data_[i] += other.data_[i];
+  return *this;
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr Matrix<rows, cols>& Matrix<rows, cols>::operator-=(const Matrix<rows, cols>& other) {
+  for (std::size_t i = 0; i != rows; ++i)
+    data_[i] -= other.data_[i];
+  return *this;
+}
+
+template <std::size_t rows, std::size_t cols> template <std::size_t span>
+constexpr Matrix<rows, span>& Matrix<rows, cols>::operator*=(const Matrix<cols, span>& other) {
+  Matrix<rows, span> result;
+  for (std::size_t i = 0; i != rows; ++i)
+    for (std::size_t j = 0; j != span; ++j)
+      for (std::size_t k = 0; k != cols; ++k)
+        result.data_[i][j] += data_[i][k] * other.data_[k][j];
+  return *this = result;
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr Matrix<rows, cols>& Matrix<rows, cols>::operator*=(float scalar) {
+  for (std::size_t i = 0; i != rows; ++i)
+    data_[i] *= scalar;
+  return *this;
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr Matrix<rows, cols> operator+(const Matrix<rows, cols>& m1, const Matrix<rows, cols>& m2) {
+  return Matrix<rows, cols>(m1) += m2;
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr Matrix<rows, cols> operator-(const Matrix<rows, cols>& m1, const Matrix<rows, cols>& m2) {
+  return Matrix<rows, cols>(m1) -= m2;
+}
+
+template <std::size_t rows, std::size_t cols, std::size_t span>
+constexpr Matrix<rows, span> operator*(const Matrix<rows, cols>& m1, const Matrix<cols, span>& m2) {
+  Matrix<rows, cols>(m1) *= m2;
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr Matrix<rows, cols> operator*(const Matrix<rows, cols>& matrix, float scalar) {
+  return Matrix<rows, cols>(matrix) *= scalar;
+}
+
+template <std::size_t rows, std::size_t cols>
+constexpr Vector<rows - 1> operator*(const Matrix<rows, cols>& matrix, const Vector<rows - 1>& vector) {
+  static_assert(rows > 1, "Must not be called on a vector");
+  Vector<rows> w(vector);
+  w.back() = 1.0f;
+
+  Vector<rows - 1> result;
+  for (std::size_t i = 0; i + 1 < rows; ++i)
+    for (std::size_t k = 0; k != cols; ++k)
+      result[i] += matrix[i][k] * w[k];
+
+  return result;
+}
+
+template <std::size_t rows, std::size_t cols>
+std::istream& operator>>(std::istream& is, Matrix<rows, cols>& matrix) {
+  for (std::size_t i = 0; i != rows; ++i)
+    is >> matrix.data_[i];
+  return is;
+}
+
+template <std::size_t rows, std::size_t cols>
+std::ostream& operator<<(std::ostream& os, const Matrix<rows, cols>& matrix) {
+  for (std::size_t i = 0; i != rows; ++i) {
+    os << (i ? ' ' : '(') << matrix.data_[i] << (i == rows - 1 ? ")" : ",\n");
+  }
+  return os;
+}
+
+#endif  // MATRIX_H_
--- math3d/vector.h
+++ math3d/vector.h
@@ -1,11 +1,13 @@
 #ifndef VECTOR_H_
 #define VECTOR_H_
 
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 #include <initializer_list>
 #include <istream>
 #include <ostream>
+#include <stdexcept>
 #include "global.h"
 
 template <std::size_t size>
@@ -24,14 +26,40 @@ class Vector {
   static_assert(size != 0, "Zero dimension vector does not exist");
 
  public:
+  using pointer = float*;
+  using const_pointer = const float*;
+  using reference = float&;
+  using const_reference = const float&;
+  using iterator = float*;
+  using const_iterator = const float*;
+
   constexpr Vector() noexcept;
   constexpr Vector(const std::initializer_list<float>& list) noexcept;
+  explicit Vector(float scalar) noexcept;
 
-  float* data();
-  constexpr const float* data() const;
+  template <std::size_t other_size>
+  constexpr explicit Vector(const Vector<other_size>& other) noexcept;
 
-  float& operator[](std::size_t index);
-  constexpr float operator[](std::size_t index) const;
+  pointer data();
+  constexpr const_pointer data() const;
+
+  reference operator[](std::size_t index);
+  constexpr const_reference operator[](std::size_t index) const;
+
+  reference at(std::size_t index);
+  constexpr const_reference at(std::size_t index) const;
+
+  reference front();
+  constexpr const_reference front() const;
+
+  reference back();
+  constexpr const_reference back() const;
+
+  iterator begin();
+  constexpr const_iterator begin() const;
+
+  iterator end();
+  constexpr const_iterator end() const;
 
   template <bool EnableBool = true> constexpr typename std::enable_if<Is2or3or4(size) && EnableBool, float>::type x() const;
   template <bool EnableBool = true> constexpr typename std::enable_if<Is2or3or4(size) && EnableBool, float>::type y() const;
@@ -47,8 +75,8 @@ class Vector {
   constexpr bool operator!=(const Vector<size>& other) const;
   constexpr bool static FuzzyCompare(const Vector<size>& v1, const Vector<size>& v2);
 
-  bool IsNull() const;
-  bool IsNormalized() const;
+  constexpr bool IsNull() const;
+  constexpr bool IsNormalized() const;
 
   float length() const;
 
@@ -57,35 +85,35 @@ class Vector {
   constexpr static Vector<size> CrossProduct(const Vector<size>& v1, const Vector<size>& v2) = delete;
   constexpr static float SkewProduct(const Vector<size>& v1, const Vector<size>& v2) = delete;
 
-  Vector<size> Normalized() const;
-  void Normalize();
+  constexpr Vector<size> Normalized() const;
+  constexpr void Normalize();
 
-  static Vector<size> Normal(const Vector<size>& v1, const Vector<size>& v2) = delete;
-  static Vector<size> Normal(const Vector<size>& v1, const Vector<size>& v2, const Vector<size>& v3) = delete;
+  static constexpr Vector<size> Normal(const Vector<size>& v1, const Vector<size>& v2) = delete;
+  static constexpr Vector<size> Normal(const Vector<size>& v1, const Vector<size>& v2, const Vector<size>& v3) = delete;
 
-  float DistanceToPoint(const Vector<size>& point) const;
-  float DistanceToLine(const Vector<size>& point, const Vector<size>& direction) const;
-  float DistanceToPlane(const Vector<size>& plane, const Vector<size>& normal) const = delete;
-  float DistanceToPlane(const Vector<size>& plane1, const Vector<size>& plane2, const Vector<size>& plane3) const = delete;
+  constexpr float DistanceToPoint(const Vector<size>& point) const;
+  constexpr float DistanceToLine(const Vector<size>& point, const Vector<size>& direction) const;
+  constexpr float DistanceToPlane(const Vector<size>& plane, const Vector<size>& normal) const = delete;
+  constexpr float DistanceToPlane(const Vector<size>& plane1, const Vector<size>& plane2, const Vector<size>& plane3) const = delete;
 
-  Vector<size>& operator+=(const Vector<size>& other);
-  Vector<size>& operator-=(const Vector<size>& other);
-  Vector<size>& operator*=(float factor);
-  Vector<size>& operator*=(const Vector<size>& other);
-  Vector<size>& operator/=(float divisor);
-  Vector<size>& operator/=(double divisor);
-  Vector<size>& operator/=(const Vector<size>& other);
+  constexpr Vector<size>& operator+=(const Vector<size>& other);
+  constexpr Vector<size>& operator-=(const Vector<size>& other);
+  constexpr Vector<size>& operator*=(float factor);
+  constexpr Vector<size>& operator*=(const Vector<size>& other);
+  constexpr Vector<size>& operator/=(float divisor);
+  constexpr Vector<size>& operator/=(double divisor);
+  constexpr Vector<size>& operator/=(const Vector<size>& other);
 
-  template <std::size_t size_> friend Vector<size_> operator+(const Vector<size_>& v1, const Vector<size_>& v2);
-  template <std::size_t size_> friend Vector<size_> operator+(const Vector<size_>& vector);
-  template <std::size_t size_> friend Vector<size_> operator-(const Vector<size_>& v1, const Vector<size_>& v2);
-  template <std::size_t size_> friend Vector<size_> operator-(const Vector<size_>& vector);
-  template <std::size_t size_> friend Vector<size_> operator*(const Vector<size_>& vector, float factor);
-  template <std::size_t size_> friend Vector<size_> operator*(float factor, const Vector<size_>& vector);
-  template <std::size_t size_> friend Vector<size_> operator*(const Vector<size_>& v1, const Vector<size_>& v2);
-  template <std::size_t size_> friend Vector<size_> operator/(const Vector<size_>& vector, float divisor);
-  template <std::size_t size_> friend Vector<size_> operator/(const Vector<size_>& vector, double divisor);
-  template <std::size_t size_> friend Vector<size_> operator/(const Vector<size_>& v1, const Vector<size_>& v2);
+  template <std::size_t size_> friend constexpr Vector<size_> operator+(const Vector<size_>& v1, const Vector<size_>& v2);
+  template <std::size_t size_> friend constexpr Vector<size_> operator+(const Vector<size_>& vector);
+  template <std::size_t size_> friend constexpr Vector<size_> operator-(const Vector<size_>& v1, const Vector<size_>& v2);
+  template <std::size_t size_> friend constexpr Vector<size_> operator-(const Vector<size_>& vector);
+  template <std::size_t size_> friend constexpr Vector<size_> operator*(const Vector<size_>& vector, float factor);
+  template <std::size_t size_> friend constexpr Vector<size_> operator*(float factor, const Vector<size_>& vector);
+  template <std::size_t size_> friend constexpr Vector<size_> operator*(const Vector<size_>& v1, const Vector<size_>& v2);
+  template <std::size_t size_> friend constexpr Vector<size_> operator/(const Vector<size_>& vector, float divisor);
+  template <std::size_t size_> friend constexpr Vector<size_> operator/(const Vector<size_>& vector, double divisor);
+  template <std::size_t size_> friend constexpr Vector<size_> operator/(const Vector<size_>& v1, const Vector<size_>& v2);
 
   template <std::size_t size_> friend std::istream& operator>>(std::istream& is, Vector<size_>& vector);
   template <std::size_t size_> friend std::ostream& operator<<(std::ostream& os, const Vector<size_>& vector);
@@ -100,29 +128,96 @@ constexpr Vector<size>::Vector() noexcept: data_{} {}
 template <std::size_t size>
 constexpr Vector<size>::Vector(const std::initializer_list<float>& list) noexcept: data_{} {
   std::copy(list.begin(), list.end(), data_);
-
 }
 
 template <std::size_t size>
-float* Vector<size>::data() {
+Vector<size>::Vector(float scalar) noexcept: data_{} {
+  std::fill(data_, data_ + size, scalar);
+}
+
+template <std::size_t size> template <std::size_t other_size>
+constexpr Vector<size>::Vector(const Vector<other_size>& other) noexcept: data_{} {
+  if (size <= other_size)
+    std::copy(other.begin(), other.begin() + size, data_);
+  else
+    std::copy(other.begin(), other.end(), data_);
+}
+
+
+template <std::size_t size>
+typename Vector<size>::pointer Vector<size>::data() {
   return data_;
 }
 
 template <std::size_t size>
-constexpr const float* Vector<size>::data() const {
+constexpr typename Vector<size>::const_pointer Vector<size>::data() const {
   return data_;
 }
 
 template <std::size_t size>
-float& Vector<size>::operator[](std::size_t index) {
+typename Vector<size>::reference Vector<size>::operator[](std::size_t index) {
   assert(index < size);
   return data_[index];
 }
 
 template <std::size_t size>
-constexpr float Vector<size>::operator[](std::size_t index) const {
+constexpr typename Vector<size>::const_reference Vector<size>::operator[](std::size_t index) const {
   assert(index < size);
   return data_[index];
+}
+
+template <std::size_t size>
+typename Vector<size>::reference Vector<size>::at(std::size_t index) {
+  if (index >= size)
+    throw std::out_of_range("Out of range");
+  return data_[index];
+}
+
+template <std::size_t size>
+constexpr typename Vector<size>::const_reference Vector<size>::at(std::size_t index) const {
+  if (index >= size)
+    throw std::out_of_range("Out of range");
+  return data_[index];
+}
+
+template <std::size_t size>
+typename Vector<size>::reference Vector<size>::front() {
+  return data_[0];
+}
+
+template <std::size_t size>
+constexpr typename Vector<size>::const_reference Vector<size>::front() const {
+  return data_[0];
+}
+
+template <std::size_t size>
+typename Vector<size>::reference Vector<size>::back() {
+  return data_[size - 1];
+}
+
+template <std::size_t size>
+constexpr typename Vector<size>::const_reference Vector<size>::back() const {
+  return data_[size - 1];
+}
+
+template <std::size_t size>
+typename Vector<size>::iterator Vector<size>::begin() {
+  return data_;
+}
+
+template <std::size_t size>
+constexpr typename Vector<size>::const_iterator Vector<size>::begin() const {
+  return data_;
+}
+
+template <std::size_t size>
+typename Vector<size>::iterator Vector<size>::end() {
+  return data_ + size;
+}
+
+template <std::size_t size>
+constexpr typename Vector<size>::const_iterator Vector<size>::end() const {
+  return data_ + size;
 }
 
 template <std::size_t size> template <bool EnableBool>
@@ -192,7 +287,7 @@ constexpr bool Vector<size>::FuzzyCompare(const Vector<size>& v1, const Vector<s
 }
 
 template <std::size_t size>
-bool Vector<size>::IsNull() const {
+constexpr bool Vector<size>::IsNull() const {
   for (std::size_t i = 0; i != size; ++i)
     if (data_[i] != 0.0f)
       return false;
@@ -200,7 +295,7 @@ bool Vector<size>::IsNull() const {
 }
 
 template <std::size_t size>
-bool Vector<size>::IsNormalized() const {
+constexpr bool Vector<size>::IsNormalized() const {
   return FuzzyIsNull(DotProduct(*this, *this) - 1.0);
 }
 
@@ -240,7 +335,7 @@ constexpr float Vector<2>::SkewProduct(const Vector<2>& v1, const Vector2 &v2) {
 }
 
 template <std::size_t size>
-Vector<size> Vector<size>::Normalized() const {
+constexpr Vector<size> Vector<size>::Normalized() const {
   const double sqr_len = DoubleDotProduct(*this, *this);
   if (FuzzyIsNull(sqr_len))
     return Vector<size>();
@@ -251,7 +346,7 @@ Vector<size> Vector<size>::Normalized() const {
 }
 
 template <std::size_t size>
-void Vector<size>::Normalize() {
+constexpr void Vector<size>::Normalize() {
   const double sqr_len = DoubleDotProduct(*this, *this);
   if (FuzzyIsNull(sqr_len) || (FuzzyIsNull(sqr_len - 1.0)))
     return;
@@ -260,22 +355,22 @@ void Vector<size>::Normalize() {
 }
 
 template <>
-Vector<3> Vector<3>::Normal(const Vector<3>& v1, const Vector<3>& v2) {
+constexpr Vector<3> Vector<3>::Normal(const Vector<3>& v1, const Vector<3>& v2) {
   return CrossProduct(v1, v2).Normalized();
 }
 
 template <>
-Vector<3> Vector<3>::Normal(const Vector<3>& v1, const Vector<3>& v2, const Vector<3>& v3) {
+constexpr Vector<3> Vector<3>::Normal(const Vector<3>& v1, const Vector<3>& v2, const Vector<3>& v3) {
   return CrossProduct(v2 - v1, v3 - v1).Normalized();
 }
 
 template <std::size_t size>
-float Vector<size>::DistanceToPoint(const Vector<size>& point) const {
+constexpr float Vector<size>::DistanceToPoint(const Vector<size>& point) const {
   return (*this - point).length();
 }
 
 template <std::size_t size>
-float Vector<size>::DistanceToLine(const Vector<size>& point, const Vector<size>& direction) const {
+constexpr float Vector<size>::DistanceToLine(const Vector<size>& point, const Vector<size>& direction) const {
   if (direction.IsNull())
     return (*this - point).length();
   const Vector<size> p = point + DotProduct(*this - point, direction) * direction;
@@ -283,114 +378,114 @@ float Vector<size>::DistanceToLine(const Vector<size>& point, const Vector<size>
 }
 
 template <>
-float Vector<3>::DistanceToPlane(const Vector<3>& plane, const Vector<3>& normal) const {
+constexpr float Vector<3>::DistanceToPlane(const Vector<3>& plane, const Vector<3>& normal) const {
   return DotProduct(*this - plane, normal);
 }
 
 template <>
-float Vector<3>::DistanceToPlane(const Vector<3>& plane1, const Vector<3>& plane2, const Vector<3>& plane3) const {
+constexpr float Vector<3>::DistanceToPlane(const Vector<3>& plane1, const Vector<3>& plane2, const Vector<3>& plane3) const {
   const Vector<3> n = Normal(plane2 - plane1, plane3 - plane1);
   return DotProduct(*this - plane1, n);
 }
 
 template <std::size_t size>
-Vector<size>& Vector<size>::operator+=(const Vector<size>& other) {
+constexpr Vector<size>& Vector<size>::operator+=(const Vector<size>& other) {
   for (std::size_t i = 0; i != size; ++i)
     data_[i] += other.data_[i];
   return *this;
 }
 
 template <std::size_t size>
-Vector<size>& Vector<size>::operator-=(const Vector<size>& other) {
+constexpr Vector<size>& Vector<size>::operator-=(const Vector<size>& other) {
   for (std::size_t i = 0; i != size; ++i)
     data_[i] -= other.data_[i];
   return *this;
 }
 
 template <std::size_t size>
-Vector<size>& Vector<size>::operator*=(float factor) {
+constexpr Vector<size>& Vector<size>::operator*=(float factor) {
   for (std::size_t i = 0; i != size; ++i)
     data_[i] *= factor;
   return *this;
 }
 
 template <std::size_t size>
-Vector<size>& Vector<size>::operator*=(const Vector<size>& other) {
+constexpr Vector<size>& Vector<size>::operator*=(const Vector<size>& other) {
   for (std::size_t i = 0; i != size; ++i)
     data_[i] *= other.data_[i];
   return *this;
 }
 
 template <std::size_t size>
-Vector<size>& Vector<size>::operator/=(float divisor) {
+constexpr Vector<size>& Vector<size>::operator/=(float divisor) {
   for (std::size_t i = 0; i != size; ++i)
     data_[i] /= divisor;
   return *this;
 }
 
 template <std::size_t size>
-Vector<size>& Vector<size>::operator/=(double divisor) {
+constexpr Vector<size>& Vector<size>::operator/=(double divisor) {
   for (std::size_t i = 0; i != size; ++i)
     data_[i] /= divisor;
   return *this;
 }
 
 template <std::size_t size>
-Vector<size>& Vector<size>::operator/=(const Vector<size>& other) {
+constexpr Vector<size>& Vector<size>::operator/=(const Vector<size>& other) {
   for (std::size_t i = 0; i != size; ++i)
     data_[i] /= other.data_[i];
   return *this;
 }
 
 template <std::size_t size>
-Vector<size> operator+(const Vector<size>& v1, const Vector<size>& v2) {
+constexpr Vector<size> operator+(const Vector<size>& v1, const Vector<size>& v2) {
   return Vector<size>(v1) += v2;
 }
 
 template <std::size_t size>
-Vector<size> operator+(const Vector<size>& vector) {
+constexpr Vector<size> operator+(const Vector<size>& vector) {
   return vector;
 }
 
 template <std::size_t size>
-Vector<size> operator-(const Vector<size>& v1, const Vector<size>& v2) {
+constexpr Vector<size> operator-(const Vector<size>& v1, const Vector<size>& v2) {
   return Vector<size>(v1) -= v2;
 }
 
 template <std::size_t size>
-Vector<size> operator-(const Vector<size>& vector) {
+constexpr Vector<size> operator-(const Vector<size>& vector) {
   return Vector<size>() -= vector;
 }
 
 template <std::size_t size>
-Vector<size> operator*(const Vector<size>& vector, float factor) {
+constexpr Vector<size> operator*(const Vector<size>& vector, float factor) {
   return Vector<size>(vector) *= factor;
 }
 
 template <std::size_t size>
-Vector<size> operator*(float factor, const Vector<size>& vector) {
+constexpr Vector<size> operator*(float factor, const Vector<size>& vector) {
   return vector * factor;
 }
 
 template <std::size_t size>
-Vector<size> operator*(const Vector<size>& v1, const Vector<size>& v2) {
+constexpr Vector<size> operator*(const Vector<size>& v1, const Vector<size>& v2) {
   return Vector<size>(v1) *= v2;
 }
 
 template <std::size_t size>
-Vector<size> operator/(const Vector<size>& vector, float divisor) {
+constexpr Vector<size> operator/(const Vector<size>& vector, float divisor) {
   assert(divisor != 0.0f);
   return Vector<size>(vector) /= divisor;
 }
 
 template <std::size_t size>
-Vector<size> operator/(const Vector<size>& vector, double divisor) {
+constexpr Vector<size> operator/(const Vector<size>& vector, double divisor) {
   assert(divisor != 0.0f);
   return Vector<size>(vector) /= divisor;
 }
 
 template <std::size_t size>
-Vector<size> operator/(const Vector<size>& v1, const Vector<size>& v2) {
+constexpr Vector<size> operator/(const Vector<size>& v1, const Vector<size>& v2) {
   return Vector<size>(v1) /= v2;
 }
 
@@ -406,7 +501,7 @@ std::ostream& operator<<(std::ostream& os, const Vector<size>& vector) {
   os << '(';
   for (std::size_t i = 0; i + 1 < size; ++i)
     os << vector.data_[i] << ", ";
-  os << vector.data_[size - 1] << ')';
+  os << vector.back() << ')';
   return os;
 }
 
